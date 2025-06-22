bb735ca362dfec3858223836b7105fc2532e9da2
Gustavo <gustavo.leste@hotmail.com>
2025-06-22 13:11:51 -0300

    update: service worker

--- src/assets/scripts/config/launcherGame.ts
+++ src/assets/scripts/config/launcherGame.ts
@@ -4,15 +4,15 @@ import '../../styles/fonts.css';
 
 export default new Game(gameConfig);
 
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', () => {
-//       navigator.serviceWorker
-//         .register('service-worker.js')
-//         .then((registration) => {
-//           console.log('Service Worker registrado com sucesso:', registration);
-//         })
-//         .catch((err) => {
-//           console.log('Erro ao registrar o Service Worker:', err);
-//         });
-//     });
-//   }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registrado com sucesso:', registration);
+      })
+      .catch((err) => {
+        console.log('Erro ao registrar o Service Worker:', err);
+      });
+  });
+}
